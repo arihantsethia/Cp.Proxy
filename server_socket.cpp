c88f67a693c1b28937c277243e23196890ae2b95
Arihant Sethia <arihantsethia07@gmail.cim>
2014-02-10 17:32:59 +0530

    Wrapper Class for ServerSocket & ClientSocket Defined

---
+++ server_socket.cpp
@@ -0,0 +1,56 @@
+#include "server_socket.h"
+
+ServerSocket::ServerSocket(){
+	if(!Socket::create()){
+		SocketException("Server socket could not be created\n");
+	}
+}
+
+ServerSocket::ServerSocket(int port){
+	if(!Socket::create()){
+		SocketException("Server socket could not be created.\n");
+	}
+
+	if(!Socket::bind(port)){
+		SocketException("Socket couldn't bind to the port.\n");
+	}
+
+	if(!Socket::listen()){
+		SocketException("Server socket listen on the port.\n");
+	}
+}
+
+ServerSocket::~ServerSocket(){
+
+}
+
+ServerSocket ServerSocket::operator << (std::string s){
+	if(!Socket::send(s)){
+		SocketException("Couldn't write to socket.\n");
+	}
+	return this;
+}
+
+ServerSocket ServerSocket::operator >> (std::string){
+	if(!Socket::recv(s)){
+		SocketException("Couldn't read from socket.\n");
+	}
+	return this;
+}
+
+int ClientSocket::fd(){
+	return Socket::fd();
+}
+
+void ServerSocket::accept(ServerSocket& _socket){
+	if(!Socket::accept(_socket)){
+		SocketException("Couldn't accept socket.\n");
+	}
+}
+
+void ClientSocket::close(){
+	if(!Socket::close()){
+		SocketException("Couldn't close the server socket");
+	}
+}
+
